906f3a15546d41e461ce378ed535e948c36c3287
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-05-22 01:21:15 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ app.get("/",function(req,res){
     res.render("gym1");
 });
 
+app.get("/about",function(req,res){
+    res.render("about");
+});
+
+app.get("/product",function(req,res){
+    res.render("product");
+});
+
+app.get("/service",function(req,res){
+    res.render("service");
+});
+
 app.listen(process.env.PORT || 3000,function(){
     console.log("App is listenning on 3000");
 })
